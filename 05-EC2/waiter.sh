e098415524571b385dc50d958af73888ca9d8d43
mr2chowd <mr2chowd@uwaterloo.ca>
2022-06-21 18:04:08 -0400

    new

--- 05-EC2/waiter.sh
+++ 05-EC2/waiter.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-aws cloudformation create-stack --stack-name ec2test --template-body file://5.1.2.yaml
+aws cloudformation update-stack --stack-name ec2test --template-body file://5.2.1.yaml
 
 aws cloudformation wait stack-create-complete --stack-name ec2test
